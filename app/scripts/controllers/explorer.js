3f28f206e3a10d33f8aca3fe41c1d75a88801c32
Caleb Thorsteinson <caleb@thorsteinson.me>
2015-03-13 03:16:11 -0400

    Created functional navigation pane
    On the left side, there now is a pane that displays the different associations is a given folder. Additionally, there's a huge amount of styling information which is used to give it a simple look, but could be exteneded and polished to create a refined theme.

--- app/scripts/controllers/explorer.js
+++ app/scripts/controllers/explorer.js
@@ -14,12 +14,14 @@ angular.module('itemMirrorAngularDemoApp')
   	init.then(function() {
       $scope.mirror = itemMirror;
       $scope.associations = itemMirror.associations;
+      $scope.selectedAssoc = null;
 
       // This needs to be called after the service updates the associations.
       // Angular doesn't watch the scope of the service's associations, so any
       // updates don't get propogated to the front end.
       function assocScopeUpdate() {
         $scope.associations = itemMirror.associations;
+        $scope.selectedAssoc = null;
        }
 
       $scope.deleteAssoc = function(guid) {
@@ -30,6 +32,21 @@ angular.module('itemMirrorAngularDemoApp')
       $scope.navigate = function(guid) {
         itemMirror.navigateMirror(guid).
         then(assocScopeUpdate);
+      };
+
+      $scope.previous = function() {
+        itemMirror.previous().
+        then(assocScopeUpdate);
+      };
+
+      // Only one association is ever selected at a time. It has the boolean
+      // selected property, to allow for unique styling
+      $scope.select = function(assoc) {
+        if ($scope.selectedAssoc) {
+          $scope.selectedAssoc.selected = false;
+        }
+        $scope.selectedAssoc = assoc;
+        $scope.selectedAssoc.selected = true;
       };
     });
   });
